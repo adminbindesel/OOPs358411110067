71841344c55ef1db2fb012046d0898e8fbba2ebe
adminbindesel <kritsadakeawkuea1@gmail.com>
2018-11-19 16:35:48 +0700

    Lab l commit

--- src/lab1/intro.java
+++ src/lab1/intro.java
@@ -1,4 +1,24 @@
 package lab1;
 
 public class intro {
-}
+    public static void main(String[] args){
+
+        System.out.println("KRITSADA");
+        System.out.println("kritsadakeawkuea1@gmail.com");
+        System.out.println("information system");
+        System.out.println("RMUSV");
+
+
+
+
+    }//main
+
+
+
+}//class
+
+
+
+
+
+
