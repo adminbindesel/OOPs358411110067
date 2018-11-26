c5e59d2ae4690823c0f2753086e45ba5c2684913
LabCom_4 <kritsadakeawkuea1@gmail.com>
2018-11-26 14:34:49 +0700

    lab2: Variables

---
+++ src/lab2/ex1.java
@@ -0,0 +1,47 @@
+package lab2;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+        //integer
+        int a;
+        a = 10;
+        System.out.println(a);
+        int b = 20;
+        System.out.println(b);
+        int c = a+b;
+        System.out.println("ผลรวมของ a และ b = "+c);
+
+        //real number
+        double d;
+        d = 5.5;
+        d = d*10; // d *=10;
+        System.out.print(d);
+
+        //charactrt
+        char e = '<' ;
+        System.out.println(e);
+
+        //String
+        String str = "KRITSADA KEAWKUEA";
+        System.out.println("My name is \t"+str);
+        System.out.println(str.length());
+        System.out.println(str.toUpperCase());
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}//class
