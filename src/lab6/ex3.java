5b3e45760dd6ea97bb8d59262824713f736a9226
adminbindesel <kritsadakeawkuea1@gmail.com>
2018-12-24 15:41:13 +0700

    leb6

---
+++ src/lab6/ex3.java
@@ -0,0 +1,11 @@
+package lab6;
+
+public class ex3 {
+    public static void main(String[] args) {
+        String myMsg = "I am working at RUTS 2018";
+        StringBuffer strbuf = new StringBuffer(myMsg);
+
+        System.out.println(strbuf);
+        System.out.println(strbuf.reverse());
+    }
+}
